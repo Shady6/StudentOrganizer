f43b6e9f2298d3e19a509af25d45b59a4bb528aa
Mikolaj <mikolaj.mikolaj@gmail.com>
2021-03-18 22:30:13 +0100

    (Commit message) Added feed and post classes

---
+++ src/StudentOrganizer.Core/Models/Feed.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Buffers.Binary;
+
+namespace StudentOrganizer.Core.Models
+{
+	public class Feed : Entity
+	{
+		public List<Post> Posts { get; set; }		
+	}
+
+	public class Post : Entity
+	{
+		public string Content { get; set; }
+	}
+}
