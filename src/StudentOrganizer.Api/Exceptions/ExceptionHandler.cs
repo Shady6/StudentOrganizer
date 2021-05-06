e9532b655038c80525466c51677c8e162a667285
Shady06 <mikolaj.mikolaj@gmail.com>
2021-05-06 19:37:29 +0200

    Return message instead of throwing ex in ExceptionHandler

--- src/StudentOrganizer.Api/Exceptions/ExceptionHandler.cs
+++ src/StudentOrganizer.Api/Exceptions/ExceptionHandler.cs
@@ -6,49 +6,54 @@ using StudentOrganizer.Core.Common;
 
 namespace StudentOrganizer.Api.Exceptions
 {
-    public class ExceptionHandler
-    {
-        private readonly RequestDelegate next;
+	public class ExceptionHandler
+	{
+		private readonly RequestDelegate next;
 
-        public ExceptionHandler(RequestDelegate next)
-        {
-            this.next = next;
-        }
+		public ExceptionHandler(RequestDelegate next)
+		{
+			this.next = next;
+		}
 
-        public async Task Invoke(HttpContext context)
-        {
-            try
-            {
-                await next(context);
-            }
-            catch (Exception exception)
-            {
-                await HandleExceptionAsync(context, exception);
-            }
-        }
+		public async Task Invoke(HttpContext context)
+		{
+			try
+			{
+				await next(context);
+			}
+			catch (Exception exception)
+			{
+				await HandleExceptionAsync(context, exception);
+			}
+		}
 
-        private Task HandleExceptionAsync(HttpContext context, Exception exception)
-        {
-            var errorCode = AppErrorCode.DEFAULT_ERROR;            
+		private Task HandleExceptionAsync(HttpContext context, Exception exception)
+		{
+			var errorCode = AppErrorCode.DEFAULT_ERROR;
 			if (exception is AppException e)
 			{
-                errorCode = e.ErrorCode;
-                if (e.ErrorCode == AppErrorCode.CANT_DO_THAT)
-                    context.Response.StatusCode = (int)HttpStatusCode.Forbidden;
-                else
-                    context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
-            }
+				errorCode = e.ErrorCode;
+				if (e.ErrorCode == AppErrorCode.CANT_DO_THAT)
+					context.Response.StatusCode = (int)HttpStatusCode.Forbidden;
+				else
+					context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+			}
 
-            var errorResponse = new ErrorResponse
-            {
-                Message = exception.Message,
-                StatusCode = errorCode
-            }.ToString();
+			var errorResponse = new ErrorResponse
+			{
+				Message = exception.Message,
+				StatusCode = errorCode
+			}.ToString();
 
-            throw new Exception($"\n___ PRODUCTION ERROR RESPONSE ___\n{errorResponse}\n___DEVELOPMENT ERROR RESPONSE___\n{exception.Message}", exception);
-
-            // TODO uncomment when development is done
-            // return context.Response.WriteAsync(errorResponse);
-        }
-    }
+			return context.Response.WriteAsync(
+				$"\n___ PRODUCTION ERROR RESPONSE ___\n" +
+				$"{errorResponse}\n" +
+				$"___ DEVELOPMENT ERROR RESPONSE ___\n" +
+				$"{exception.Message}\n" +
+				$"{exception.InnerException?.Message ?? ""}\n" +
+				$"{exception.StackTrace} \n" +
+				$"{exception.InnerException?.StackTrace ?? ""}"
+				 );
+		}
+	}
 }
